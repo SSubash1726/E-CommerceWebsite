a16b9b5f4429b1b2b6507f443ec0304f85be1ea4
subash <subashconner23@gmail.com>
2025-06-05 12:28:48 +0530

    initial commit

---
+++ src/app/component/product-view/product-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-view',
+  standalone: false,
+  templateUrl: './product-view.component.html',
+  styleUrl: './product-view.component.css'
+})
+export class ProductViewComponent {
+
+}
